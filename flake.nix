1dcccb1c1617482d7758155c8e82e09de5188acf
Jente Hidskes Ankarberg <dev@hjdskes.nl>
2026-07-02 18:02:02 +0200

    Add treefmt-nix for source tree wide formatting

--- flake.nix
+++ flake.nix
@@ -2,19 +2,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
 
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
+      treefmt-nix,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        treefmt = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
       in
       {
         devShells.default =
@@ -27,8 +32,15 @@
               clj-kondo
               mosquitto
               nixfmt
+              treefmt.config.build.wrapper
             ];
           };
+
+        checks = {
+          formatting = treefmt.config.build.check self;
+        };
+
+        formatter = treefmt.config.build.wrapper;
       }
     );
 }
