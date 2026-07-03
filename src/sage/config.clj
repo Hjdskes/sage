b052156e29834ffa3956118ccb3d502c255abcde
Jente Hidskes Ankarberg <dev@hjdskes.nl>
2026-07-03 22:28:03 +0200

    Catch case where config.edn is not on the classpath

--- src/sage/config.clj
+++ src/sage/config.clj
@@ -1,5 +1,5 @@
 (ns sage.config
-  (:refer-clojure :exclude [get key])
+  (:refer-clojure :exclude [get key resolve])
   (:require
     [aero.core :as aero]
     [clojure.java.io :as io]))
@@ -9,15 +9,22 @@
 (def ^:private *config
   (atom nil))
 
+(defn- resolve
+  [source]
+  (or (io/resource source)
+      (throw (ex-info "Config not found on classpath" {:source source}))))
+
 (defn init!
   "Load and store the config for the given profile.
 
    Must be called once at startup before calling any other function that works
    on the config, such as `get` below.
 
-   Calling this function again replaces the config (useful in tests to switch profiles)."
+   Calling this function again replaces the config (useful in tests to switch profiles).
+
+   Throws if the file `config.edn` is not on the class path."
   [profile]
-  (reset! *config (aero/read-config (io/resource "config.edn") {:profile profile})))
+  (reset! *config (aero/read-config (resolve "config.edn") {:profile profile})))
 
 (defn get
   "Returns the value for the given key from the initialised config."
